ef484620a9ac95475e21c657ea1162166f949681
adil mae <itsadilhadid@gmail.com>
2023-09-29 04:47:38 +0100

    my server cors works only with '*' for now

--- src/App.jsx
+++ src/App.jsx
@@ -4,70 +4,61 @@ import React, { useState, useEffect, useRef } from "react";
 
 import axios from "axios";
 
+function App() {
+  const [isUpdated, setIsUpadted] = useState(false);
+  const [cards, setCards] = useState([]);
 
-function App()
-{
-    const [isUpdated, setIsUpadted] = useState(false);
-    const [cards, setCards] = useState([]);
-
-    const getAndStoreData = async () =>
-    {
-        try
-        {
-            const response = await axios.get("http://localhost:3000/api/cards");
-            setCards(response.data);
-        }
-        catch (error)
-        {
-            console.log(error.message);
-        }
-    };
-
-    const handleDeleteCard = async (e, cardId) =>
-    {
-        e.preventDefault();
-
-        try
-        {
-            await axios.delete(`http://localhost:3000/api/cards/${cardId}`);
-            getAndStoreData();
-        } catch (error)
-        {
-            alert(error);
-        }
-
-
+  const getAndStoreData = async () => {
+    try {
+      const response = await axios.get(
+        "https://posting-system-backend.onrender.com/api/cards/"
+      );
+      setCards(response.data);
+      console.log("this respons ->", response.data);
+      console.log("this cards ->", cards);
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
+  const handleDeleteCard = async (e, cardId) => {
+    e.preventDefault();
 
-    useEffect(() =>
-    {
-        getAndStoreData();
+    try {
+      await axios.delete(
+        `https://posting-system-backend.onrender.com/api/cards/${cardId}`
+      );
+      getAndStoreData();
+    } catch (error) {
+      alert(error);
+    }
+  };
 
+  useEffect(() => {
+    getAndStoreData();
+  }, []);
 
-    }, []);
+  console.log("here is cards -> ", cards);
 
-    console.log("here is cards -> ", cards);
+  return (
+    <div className="">
+      <div>
+        <PostNewCard getAndStoreData={getAndStoreData} />
+      </div>
 
-    return (
-        <div className="">
-            <div>
-                <PostNewCard getAndStoreData={getAndStoreData} />
-            </div>
-
-            <div className="flex flex-col-reverse">
-                {cards.map((card, index) => (
-                    <AnimalCard key={index} animal={card} handleDeleteCard={handleDeleteCard} />
-                ))}
-            </div>
-        </div>
-    );
+      <div className="flex flex-col-reverse">
+        {cards.map((card, index) => (
+          <AnimalCard
+            key={index}
+            animal={card}
+            handleDeleteCard={handleDeleteCard}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
 
-
-
 // ------------------------------------------------------------------------------------------
-
-
